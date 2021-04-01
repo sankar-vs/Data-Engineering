eca1673fcc0e085b3fd68dd00f50b935b58942aa
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-02 00:26:34 +0530

    [Sankar]Refactor: Exception and Comments

--- Basic Python Programs/HarmonicNumber.py
+++ Basic Python Programs/HarmonicNumber.py
@@ -1,12 +1,56 @@
-import sys
-def test_not_zero(x):       # Ensure it is a positive Integer
-    if int(x) != 0: 
+'''
+@Author: Sankar
+@Date: 2021-03-31 19:06:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-01 00:18:09
+@Title : HarmonicNumber
+'''
+def test_positive_integer(x):
+    '''
+    Description:
+        Check if the given number is greater than 0
+    Parameter:
+        x (str): Input from User
+    Return:
+        x (int)): returns if value is greater than 0
+    '''   
+    if int(x) > 0:
         return int(x)  
     else:
-        sys.exit("Enter number greater that zero")
-num = test_not_zero(input("Enter a number: "))
-harmonic = 0
-for count in range(1, num+1, 1):
-    div = (1/count)
-    harmonic += div
-print("The {}th Harmonic number: {}".format(num, harmonic))
+        print("Enter number greater that zero")
+
+def get_input():
+    '''
+    Description:
+        Get input from the User
+    Parameter:
+        None
+    Return:
+        num (int)): returns input from the user
+    '''   
+    while True:
+        try:
+            num = test_positive_integer(input("Enter a number: "))
+            return num
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...")
+
+def calculate_harmonic(num):
+    '''
+    Description:
+        Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N
+    Parameter:
+        num (int): Input from User
+    Return:
+        None
+    '''
+    try:
+        harmonic = 0
+        for count in range(1, num+1, 1):
+            div = (1/count)
+            harmonic += div
+        print("The {}th Harmonic number: {}".format(num, harmonic))
+    except TypeError:
+        print("Check input")    
+
+calculate_harmonic(get_input())

13c55b2ad3002561d2d88f40bfabb0e4ffb40117
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-01 20:36:26 +0530

    [Sankar]Added: Exceptions and comments

--- Functional Programs/2DArray.py
+++ Functional Programs/2DArray.py
@@ -1,26 +1,71 @@
-import sys
-def test_range_int(x):       # Ensure it is a positive Integer
-    if (int(x) > 0): 
+'''
+@Author: Sankar
+@Date: 2021-04-01 09:06:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-01 20:34:09
+@Title : 2DArray
+'''
+def test_positive_integer(x):
+    '''
+    Description:
+        Check if the given number is greater than 0
+    Parameter:
+        x (int)): A integer
+    Return:
+        x (int)): returns if value is greater than 0
+    '''    
+    if int(x) > 0: 
         return int(x)  
     else:
-        sys.exit("Enter Positive Integer")
-# To get the elements in the array
-def create_array(rows, columns):
-    array = [[0 for i in range(rows)] for j in range(columns)]
-    for i in range(rows):
-        for j in range(columns):
-            array[i][j] = int(input("Enter element: "))
-    return array
-# To print the array
-def print_array(array):
-    rows = len(array)
-    columns = len(array[0])
-    for i in range(rows):
-        for j in range(columns):
-            print(array[i][j], end = " ")
-        print()
+        print("Enter number greater that zero")
 
-row = test_range_int(input("Enter number of rows: "))
-column = test_range_int(input("Enter number of columns: "))
-array = create_array(row, column)  
-print_array(array)
+def create_matrix(rows, columns):
+    '''
+    Description:
+        Create a 2D array from the given rows and columns
+    Parameter:
+        rows (int)): A integer
+        columns (int)): A integer
+    Return:
+        matrix (list[][]): matrix populated by the user
+    '''
+    matrix = []
+    try:
+        for i in range(rows):
+            a = []
+            for j in range(columns):
+                a.append(int(input("Enter element: ")))
+            matrix.append(a)    
+        return matrix
+    except TypeError:
+        print("Check the values for rows and columns")  
+
+def print_matrix(matrix):
+    '''
+    Description:
+        Prints the given 2D array
+    Parameter:
+        matrix (list[][]): Integer elements array
+    Return:
+        None
+    '''
+    try:
+        rows = len(matrix)
+        columns = len(matrix[0])
+        for i in range(rows):
+            for j in range(columns):
+                print(matrix[i][j], end = " ")
+            print()
+    except TypeError:
+        print("Check the values for rows and columns")
+
+while True:
+    try:
+        row = test_positive_integer(input("Enter number of rows: "))
+        column = test_positive_integer(input("Enter number of columns: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+array = create_matrix(row, column)  
+print_matrix(array)

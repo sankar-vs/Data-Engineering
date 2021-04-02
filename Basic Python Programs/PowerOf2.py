a318e03551dbe08937c570bdbe95fe6cc1061cf4
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-02 10:41:39 +0530

    [Sankar]Refactor:Use Regex for getting input from user

--- Basic Python Programs/PowerOf2.py
+++ Basic Python Programs/PowerOf2.py
@@ -1,21 +1,49 @@
-import sys
-def test_range_int(x): 
+'''
+@Author: Sankar
+@Date: 2021-03-31 19:06:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-02 10:39:09
+@Title : PowerOf2
+'''
+import re
+def num_regex():
     '''
     Description:
-        Check if the given number is greater than 0 and less than 31
+        Get input from user, check whether the input is matching with the pattern
+        expression, if True then print return
     Parameter:
-        x (str): Input from User
+        None
     Return:
-        x (int)): returns if value is greater than 0 and less than 31
+        None
     '''
-    if (int(x) > 0 and int(x) < 31): 
-        return int(x)  
-    else:
-        print("Enter number in range between 1 and 30")
-num = test_range_int(input("Enter a number: "))
-for count in range(1, pow(2, num)+1,1):
-    table = count * 2
-    if( table > pow(2, num)):
-        break
-    else:
-        print("2 * {} = {}".format(count, table))
+    while True:
+        try:
+            num = input("Enter a number: ")
+            pattern = "([1-9]|[12][0-9]|30)$"
+            result = re.match(pattern, num)
+            if (result):
+                return int(num)
+        except:
+            pass
+        print("Only numerics allowed 1-31")  
+
+def tables_of2(num):
+    '''
+    Description:
+        Prints a table of the powers of 2 that are less than or equal to 2^N.
+    Parameter:
+        num (int)): input from the user
+    Return:
+        None
+    ''' 
+    try:
+        for count in range(1, pow(2, num)+1,1):
+            table = count * 2
+            if( table > pow(2, num)):
+                break
+            else:
+                print("2 * {} = {}".format(count, table))
+    except:
+        print("Check the input")
+
+tables_of2(num_regex())

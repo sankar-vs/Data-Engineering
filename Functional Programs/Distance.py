8e826b9687e301160d2bc319c098e34540c04b4f
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-02 12:26:50 +0530

    [Sankar]Refactor:Use Regex for getting input from user

--- Functional Programs/Distance.py
+++ Functional Programs/Distance.py
@@ -2,10 +2,32 @@
 @Author: Sankar
 @Date: 2021-04-01 09:06:25
 @Last Modified by: Sankar
-@Last Modified time: 2021-04-01 20:45:37
+@Last Modified time: 2021-04-02 12:25:37
 @Title : Euclidean Distance
 '''
-def calculateEuclideanDsitance():
+import re
+def num_regex_int(x):
+    '''
+    Description:
+        Get input from user, check whether the input is matching with the pattern
+        expression, if True then return
+    Parameter:
+        x (str) : Statement to be printed to take the the inputs from user
+    Return:
+        num (int): input from user
+    '''
+    while True:
+        try:
+            num = input(x)
+            pattern = "^([+-]?[0-9]{,9})$"
+            result = re.match(pattern, num)
+            if (result):
+                return int(num)
+        except:
+            pass
+        print("Only numeric integer")
+
+def calculateEuclideanDistance():
     '''
     Description:
         Getting the x and y co-ordinates from the user and
@@ -16,15 +38,12 @@ def calculateEuclideanDsitance():
     Return:
         None
     '''
-    while True:
-        try:
-            x = int(input("Enter x co-ordinate: "))
-            y = int(input("Enter y co-ordinate: "))
-            break
-        except ValueError:
-            print("Oops!  That was no valid number.  Try again...")
+    try:
+        x = num_regex_int("Enter x co-ordinate: ")
+        y = num_regex_int("Enter y co-ordinate: ")
+        distance = pow(abs(x),abs(x))+pow(abs(y),abs(y))
+        print("Euclidean distance from the point ({}, {}) to the origin (0, 0) is: {}".format(x, y, distance))
+    except:
+        pass
 
-    distance = pow(x,x)+pow(y,y)
-    print("Euclidean distance from the point ({}, {}) to the origin (0, 0) is: {}".format(x, y, distance))
-
-calculateEuclideanDsitance()
+calculateEuclideanDistance()

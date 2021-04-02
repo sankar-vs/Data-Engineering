fff5805d58d49be4dcea7c8aa81b47c45a1073aa
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-02 09:37:14 +0530

    [Sankar]Refactor: Exceptions and comments

--- Basic Python Programs/LeapYear.py
+++ Basic Python Programs/LeapYear.py
@@ -1,18 +1,55 @@
-import sys
-def test_four_digit_number(x):       # Ensure it is a 4 digit number
+'''
+@Author: Sankar
+@Date: 2021-03-31 19:06:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-01 00:45:09
+@Title : HarmonicNumber
+'''
+def test_four_digit_number(x):
+    '''
+    Description:
+        Check if the given number is a four digit number
+    Parameter:
+        x (str): Input from User
+    Return:
+        x (int)): returns if length = 4 and if value is greater than 0
+    '''        
     if (len(x) == 4 and int(x) > 0): 
         return int(x)  
     else:
-        sys.exit("Enter 4 digit number")
+        print("Enter a 4 digit number")
 
-year = test_four_digit_number(input("Enter year: "))
-if (year % 4) == 0:
-   if (year % 100) == 0:
-       if (year % 400) == 0:
-           print("{} is a leap year".format(year))
-       else:
-           print("{} is not a leap year".format(year))
-   else:
-       print("{} is a leap year".format(year))
-else:
-   print("{} is not a leap year".format(year))
+def get_input():
+    '''
+    Description:
+        Get input from the User
+    Parameter:
+        None
+    Return:
+        year (int)): returns input from the user
+    '''   
+    while True:
+        try:
+            year = test_four_digit_number(input("Enter year: "))
+            return year
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...")
+
+def leap_year(year):
+    '''
+    Description:
+        Determine if it is a Leap Year and prints the result
+    Parameter:
+        None
+    Return:
+        None
+    '''
+    try:
+        if (( year%400 == 0) or (( year%4 == 0 ) and ( year%100 != 0))):
+            print("{} is Leap Year".format(year))
+        else:
+            print("{} is Not a Leap Year".format(year))
+    except TypeError:
+        print("Check input")
+
+leap_year(get_input())
--- Basic Python Programs/PowerOf2.py
+++ Basic Python Programs/PowerOf2.py
@@ -1,9 +1,17 @@
 import sys
-def test_range_int(x):       # Ensure it is a positive Integer
+def test_range_int(x): 
+    '''
+    Description:
+        Check if the given number is greater than 0 and less than 31
+    Parameter:
+        x (str): Input from User
+    Return:
+        x (int)): returns if value is greater than 0 and less than 31
+    '''
     if (int(x) > 0 and int(x) < 31): 
         return int(x)  
     else:
-        sys.exit("Enter number in range between 1 and 30")
+        print("Enter number in range between 1 and 30")
 num = test_range_int(input("Enter a number: "))
 for count in range(1, pow(2, num)+1,1):
     table = count * 2

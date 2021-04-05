932d28960ab1f6a57ee67db877cb95253f2e695a
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-05 09:30:10 +0530

    [Sankar]Add: Test Case for toBinary Program

---
+++ Pytest/test/test_toBinary.py
@@ -0,0 +1,29 @@
+'''
+@Author: Sankar
+@Date: 2021-04-05 09:25:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-05 09:45:09
+@Title : Test of toBinary Program
+'''
+import sys
+sys.path.append("..")
+import unittest
+from src import toBinary
+
+class TestBinary(unittest.TestCase):
+
+    def test_decimal_to_binary(self):
+        """
+        Description:
+            This method defines test case for toBinary.py file
+        Parameter:
+            None
+        Return:
+            None
+        """
+        decimal = 106
+        self.assertEqual(toBinary.DecimalToBinary(decimal), [0,1,1,0,1,0,1,0])
+        
+
+if __name__ == '__main__':
+    unittest.main()

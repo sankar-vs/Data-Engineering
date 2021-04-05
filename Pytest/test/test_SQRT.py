5acafafe400dca28b60570d8ded7f1531d309290
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-05 09:18:05 +0530

    [Sankar]Refactor: Test Case for SQRT Program

--- Pytest/test/test_SQRT.py
+++ Pytest/test/test_SQRT.py
@@ -2,45 +2,28 @@
 @Author: Sankar
 @Date: 2021-04-03 12:02:25
 @Last Modified by: Sankar
-@Last Modified time: 2021-04-03 12:54:09
+@Last Modified time: 2021-04-05 09:18:09
 @Title : Test of SQRT Program
 '''
-import pytest
+import sys
+sys.path.append("..")
+import unittest
 from src import SQRT
 
-@pytest.fixture
-def input_total():
-    '''
-    Description:
-        Set the fixture of inputs to be goven for he test case
-    Parameter:
-        None
-    Return:
-        num (int): integer input
-    '''
-    num = 25
-    return num
+class TestSQRT(unittest.TestCase):
 
-def test_regex_num():
-    '''
-    Description:
-        Test of the function num_regex_year() to check the 
-        output is equal to that of the expected result
-    Parameter:
-        None
-    Return:
-        None
-    '''
-    assert SQRT.num_regex("25") == 25
+    def test_calculate_sqrt(self):
+        """
+        Description:
+            This method defines test case for SQRT.py file
+        Parameter:
+            None
+        Return:
+            None
+        """
+        value = 25
+        self.assertEqual(SQRT.calculate_sqrt(value), 5.0)
+        
 
-def test_calculate_sqrt():
-    '''
-    Description:
-        Test of the function calculate_sqrt() to check the 
-        output is equal to that of the expected result
-    Parameter:
-        None
-    Return:
-        None
-    '''
-    assert SQRT.calculate_sqrt(input()) == 5.0
+if __name__ == '__main__':
+    unittest.main()

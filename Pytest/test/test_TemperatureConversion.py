22c0911b217d5eb6f846d18979a05c27ac1cb220
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-05 09:23:56 +0530

    [Sankar]Refactor: Test Case for Temperature Conversion Program

--- Pytest/test/test_TemperatureConversion.py
+++ Pytest/test/test_TemperatureConversion.py
@@ -5,42 +5,27 @@
 @Last Modified time: 2021-04-03 12:54:09
 @Title : Test of SQRT Program
 '''
-import pytest
+import sys
+sys.path.append("..")
+import unittest
 from src import TemperatureConversion
 
-@pytest.fixture
-def input_total():
-    '''
-    Description:
-        Set the fixture of inputs to be given for the test case
-    Parameter:
-        None
-    Return:
-        num (int): integer input
-    '''
-    num = 50
-    return num
+class TestTemperatureConversion(unittest.TestCase):
 
-def test_convert_celsius():
-    '''
-    Description:
-        Test of the function convert_to_celsius() to check the 
-        output is equal to that of the expected result
-    Parameter:
-        None
-    Return:
-        None
-    '''
-    assert TemperatureConversion.convert_to_celsius(input()) == 10.0
+    def test_conversion(self):
+        """
+        Description:
+            This method defines test case for TemperatureConversion.py file
+        Parameter:
+            None
+        Return:
+            None
+        """
+        celsius = 10
+        fahrenheit = 50
+        self.assertEqual(TemperatureConversion.convert_to_fahrenheit(celsius),50.0)
+        self.assertEqual(TemperatureConversion.convert_to_celsius(fahrenheit),10.0)
+        
 
-def test_convert_fehrenheit():
-    '''
-    Description:
-        Test of the function convert_to_celsius() to check the 
-        output is equal to that of the expected result
-    Parameter:
-        None
-    Return:
-        None
-    '''
-    assert TemperatureConversion.convert_to_fahrenheit(input()) == 122.0
+if __name__ == '__main__':
+    unittest.main()

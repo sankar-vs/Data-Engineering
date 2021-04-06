46e04cc8792ecf64974f1c33a8e4e5d7c9438ba7
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-06 08:09:43 +0530

    [Sankar]Refactor: Test raises Exception

--- Pytest/src/Binary.py
+++ Pytest/src/Binary.py
@@ -87,9 +87,8 @@ def toDecimal(list):
 	        digit = shifted_list.pop()
 	        if digit == 1:
 		        value = value + pow(2, i)
-        print("The decimal value of the number is", value)
         return value
     except:
         raise Exception
 
-toDecimal(toBinary(num_regex()))
+print("The decimal value of the number is", toDecimal(toBinary(num_regex())))

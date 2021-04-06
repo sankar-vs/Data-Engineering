f6d685955ca73264fe672e7cde2cd44210af6c10
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-06 11:37:33 +0530

    [Sankar]Add: Test Stock Account Management Program

---
+++ OOPS/test/test_stock.py
@@ -0,0 +1,29 @@
+'''
+@Author: Sankar
+@Date: 2021-04-06 09:46:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-06 10:45:09
+@Title : Stock Account Management Test
+'''
+import pytest, os
+from stock import Portfolio, Stocks
+
+def test_add_stock():
+    port = Portfolio()
+    stock = Stocks("NIFTY", 75, 283)
+    port.add_stock(stock)
+    entries = port.get_entries()
+    assert entries["NIFTY"] == stock
+
+def test_print_to_file_stock():
+    port = Portfolio()
+    stock = Stocks("NIFTY", 75, 283)
+    port.add_stock(stock)
+    port.print_file()
+    
+
+def test_read_from_file_stock():
+    port = Portfolio()
+    port.read_file()
+    entries = port.get_entries()
+    assert len(entries) == 1

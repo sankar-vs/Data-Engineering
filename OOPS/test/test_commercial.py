302a9b599d70258a0d3c3dac6500951b33cc08d1
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-07 05:40:09 +0530

    [Sankar]Refactor:Test for Commercial Program

--- OOPS/test/test_commercial.py
+++ OOPS/test/test_commercial.py
@@ -2,25 +2,49 @@
 @Author: Sankar
 @Date: 2021-04-06 09:46:25
 @Last Modified by: Sankar
-@Last Modified time: 2021-04-06 12:50:09
+@Last Modified time: 2021-04-07 05:37:09
 @Title : Test Commercial Data Processing
 '''
 import pytest, os
-from commercial import StockAccount
+from src.Commercial import StockAccount
 
 def test_buy_stock():
+    '''
+        Description:
+            Test to Buy a stock
+        Parameter:
+            None
+        Return:
+            None
+    '''
     stock = StockAccount("account")
     stock.buy("NIFTY", 10000)
     entries = stock.get_entries()
     assert entries["NIFTY"] == ["NIFTY", 10000]
 
 def test_sell_stock():
+    '''
+        Description:
+            Test to Sell a stock
+        Parameter:
+            None
+        Return:
+            None
+    '''
     stock = StockAccount("account")
     stock.sell("NIFTY", 15000)
     entries = stock.get_entries()
     assert entries["NIFTY"] == ["NIFTY", -5000]
 
 def test_save_stock():
+    '''
+        Description:
+            Test Save the details to file
+        Parameter:
+            None
+        Return:
+            None
+    '''
     stock = StockAccount("account")
     stock.save("account")
     if os.path.isfile("account.csv"):

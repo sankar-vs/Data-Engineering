b6b88f956cd965c385fcc50d4dc0df8af27d10bb
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-07 19:32:57 +0530

    [Sankar]Add: Basic_Python Program - 5

---
+++ Data Structures/Basic_Python-5.py
@@ -0,0 +1,15 @@
+'''
+@Author: Sankar
+@Date: 2021-04-07 19:18:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-07 19:25:09
+@Title : Basic_Python-4
+'''
+'''
+Write a Python program to print the calendar of a given month and year.
+Note : Use 'calendar' module.
+'''
+import calendar
+month = int(input("Enter month: "))
+year = int(input("Enter year: "))
+print(calendar.month(year, month))

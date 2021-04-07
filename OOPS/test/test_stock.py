f6461b04371275c665c3384fd131de930537e1e1
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-07 09:38:53 +0530

    [Sankar]Refactor: Test Stock Program

--- OOPS/test/test_stock.py
+++ OOPS/test/test_stock.py
@@ -9,20 +9,29 @@ import pytest, os
 from src.Stock import Portfolio, Stocks
 
 def test_add_stock():
+    '''
+    Description:
+        Adds a stock and check the dictionary is populated
+    Parameter:
+        None
+    Return:
+        None
+    '''
     port = Portfolio()
     stock = Stocks("NIFTY", 75, 283)
     port.add_stock(stock)
     entries = port.get_entries()
     assert entries["NIFTY"] == stock
 
-def test_print_to_file_stock():
-    port = Portfolio()
-    stock = Stocks("NIFTY", 75, 283)
-    port.add_stock(stock)
-    port.print_file()
-    
-
 def test_read_from_file_stock():
+    '''
+    Description:
+        Reads from a csv file and check the dictionary is populated
+    Parameter:
+        None
+    Return:
+        None
+    '''
     port = Portfolio()
     port.read_file()
     entries = port.get_entries()

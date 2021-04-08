ca4d2e6073ccc17a0111cf1e831d39158ce3820e
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 13:38:41 +0530

    [Sankar]Add: Basic_Python Program - 28

---
+++ Data Structures/Basic Program/Basic_Python-28.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-08 13:37:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-08 13:38:09
+@Title : Basic_Python-28
+'''
+'''
+Write a Python program to clear the screen or terminal.
+'''
+import os, sys
+os.system("cls")

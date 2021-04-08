ed3a44a202b11f507ff7bc4a68d2cfd20961ae63
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 07:41:28 +0530

    [Sankar]Add: Basic_Python Program - 16

---
+++ Data Structures/Basic Program/Basic_Python-16.py
@@ -0,0 +1,13 @@
+'''
+@Author: Sankar
+@Date: 2021-04-08 07:26:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-08 07:35:09
+@Title : Basic_Python-16
+'''
+'''
+Write a Python program to get the current username.
+'''
+
+import getpass
+print(getpass.getuser())

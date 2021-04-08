a03e6db06ef20612dac2e0d64ba647175bb9cd22
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 07:36:25 +0530

    [Sankar]Add: Basic_Python Program - 15

---
+++ Data Structures/Basic Program/Basic_Python-15.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-08 07:26:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-08 07:35:09
+@Title : Basic_Python-15
+'''
+'''
+Write a python program to access environment variables.
+'''
+import os
+print(os.environ['OS'])

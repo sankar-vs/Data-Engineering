81a4c28fc6eafd6bcf4cdd73475a9bee7afa2cce
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 13:32:36 +0530

    [Sankar]Add: Basic_Python Program - 26

---
+++ Data Structures/Basic Program/Basic_Python-26.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-08 13:30:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-08 13:35:09
+@Title : Basic_Python-26
+'''
+'''
+Write a Python program to count the number occurrence of a specific character in a string.
+'''
+string = "hi hema, himanshu has come home today. would you like to meet him?"
+print("Number of 'h' in the given string \n{}\n is: {}".format(string,string.count('h')))

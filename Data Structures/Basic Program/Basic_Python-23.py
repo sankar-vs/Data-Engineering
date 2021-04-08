3dd607d96b66311ab8ba6c6606858d8d514bbfba
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 13:18:25 +0530

    [Sankar]Add: Basic_Python Program - 23

---
+++ Data Structures/Basic Program/Basic_Python-23.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-08 13:13:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-08 13:20:09
+@Title : Basic_Python-23
+'''
+'''
+Write a Python program to find the available built-in modules.
+'''
+import sys
+print(sys.builtin_module_names)

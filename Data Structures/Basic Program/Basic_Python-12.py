3a742f8dfbd4893c45b6e3413b99e607fde8e3db
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 07:25:51 +0530

    [Sankar]Add: Basic_Python Program - 12

---
+++ Data Structures/Basic Program/Basic_Python-12.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-07 21:52:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-07 21:58:09
+@Title : Basic_Python-11
+'''
+'''
+Write a python program to call an external command in Python.
+'''
+import os
+os.system("dir")

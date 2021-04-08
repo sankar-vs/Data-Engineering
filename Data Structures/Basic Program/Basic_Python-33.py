3d24e6200006ffea3130e06957148ea971008423
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 14:19:23 +0530

    [Sankar]Add: Basic_Python Program - 33

---
+++ Data Structures/Basic Program/Basic_Python-33.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-08 14:15:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-08 14:58:09
+@Title : Basic_Python-33
+'''
+'''
+Write a Python program to get the users environment.
+'''
+import os
+print("Users Environment: {}".format(os.environ))

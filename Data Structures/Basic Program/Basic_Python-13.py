6cfe54a7c25dcca48fc901f98493bd6cb437659c
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-08 07:29:09 +0530

    [Sankar]Add: Basic_Python Program - 13

---
+++ Data Structures/Basic Program/Basic_Python-13.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-08 07:26:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-08 07:35:09
+@Title : Basic_Python-13
+'''
+'''
+Write a Python program to find out the number of CPUs using.
+'''
+import os
+print("Number of CPUs in the system:", os.cpu_count())

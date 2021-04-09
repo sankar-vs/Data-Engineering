64b582f4b1c5a9525d461dea65179135c68ddca9
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-09 12:35:53 +0530

    [Sankar]Add: List Program - 7

---
+++ Data Structures/List/List_Python-7.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-09 12:34:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-09 12:35:09
+@Title : List_Python-7
+'''
+'''
+Write a Python program to clone or copy a list.
+'''
+list1 = [1,2,3,9,5,10,59,78]
+clone_list = list(list1)

53b3c773992ab60454e739b2d2a08bc6b3d4132a
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-11 12:29:12 +0530

    [Sankar]Add: String Program - 1

--- Data Structures/Dictionary/Dictionary_Python-1.py
+++ Data Structures/Dictionary/Dictionary_Python-1.py
@@ -1,15 +1,12 @@
 '''
 @Author: Sankar
-@Date: 2021-04-09 08:24:25
+@Date: 2021-04-11 12:26:25
 @Last Modified by: Sankar
-@Last Modified time: 2021-04-09 08:36:09
-@Title : Dictionary_Python-1
+@Last Modified time: 2021-04-11 12:35:09
+@Title : Strings_Python-1
 '''
 '''
-Write a Python script to sort (ascending and descending) a dictionary by
-value.
+Write a Python program to calculate the length of a string.
 '''
-import operator
-dict = {"key1":5, "key2":10,"key3":1,"key4":6,"key5":4}
-print("Sort dict by Ascending: {}".format(sorted(dict.items(), key = operator.itemgetter(1))))
-print("Sort dict by Descending: {}".format(sorted(dict.items(), key = operator.itemgetter(1), reverse=True)))
+string = "Sam Current"
+print("Length of '{}': {}".format(string, len(string)))

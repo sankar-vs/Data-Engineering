aaf3379b8e900cab743b53456348d1f9e9a780e4
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-11 09:13:03 +0530

    [Sankar]Add: Tuple Program - 1

---
+++ Data Structures/Tuple/1_Create_tuple.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-11 09:10:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-11 09:12:09
+@Title : Tuple_Python-1
+'''
+'''
+Write a Python program to create a tuple.
+'''
+tuple = ("Heyya", False, 3.2, 1)
+print(tuple)

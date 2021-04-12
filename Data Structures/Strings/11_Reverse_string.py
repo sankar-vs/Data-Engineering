6d917e769099c2f89e45e278c1c11812d627cea1
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-12 07:58:06 +0530

    [Sankar]Add: String Program - 11

---
+++ Data Structures/Strings/11_Reverse_string.py
@@ -0,0 +1,12 @@
+'''
+@Author: Sankar
+@Date: 2021-04-12 07:55:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-12 07:59:09
+@Title : Strings_Python-11
+'''
+'''
+Write a Python program to reverse a string.
+'''
+str1 = 'the quick brown fox jumps over the lazy dog.'
+print(str1[::-1])

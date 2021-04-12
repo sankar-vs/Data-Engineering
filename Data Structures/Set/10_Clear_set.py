e2ed4ce98ba2cd1260e700a6a49a2f40a34c8704
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-12 08:23:05 +0530

    [Sankar]Add: Set Program - 10

---
+++ Data Structures/Set/10_Clear_set.py
@@ -0,0 +1,13 @@
+'''
+@Author: Sankar
+@Date: 2021-04-12 08:21:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-12 08:26:09
+@Title : Sets_Python-10
+'''
+'''
+Write a Python program to clear a set.
+'''
+set1 = {4,5,6,7,8,9}
+set1.clear()
+print(set1)

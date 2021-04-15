4c863e48037ae97509650a00dcbb621f24a446a4
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-15 12:31:13 +0530

    [Sankar]Add: Pandas Program - 20

---
+++ Pandas/20_Delete_column.py
@@ -0,0 +1,32 @@
+'''
+@Author: Sankar
+@Date: 2021-04-15 12:29:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-15 12:34:09
+@Title : Pandas_Python-20
+'''
+'''
+Write a Python program to delete the 'attempts' column from the DataFrame.
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew',
+'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+Values for each column will be:
+name : "Suresh", score: 15.5, attempts: 1, qualify: "yes", label: "k"
+'''
+import numpy as np
+import pandas as pd 
+
+exam = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+                                                'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam, labels)
+print(df)
+del df['attempts']
+print(df)

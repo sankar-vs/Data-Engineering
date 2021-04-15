aec7c3d00aee9a94ed5341072868fad647b4e069
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-15 11:44:39 +0530

    [Sankar]Add: Pandas Program - 11

---
+++ Pandas/11_Count_df.py
@@ -0,0 +1,29 @@
+'''
+@Author: Sankar
+@Date: 2021-04-15 11:43:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-15 11:47:09
+@Title : Pandas_Python-11
+'''
+'''
+Write a Python program to count the number of rows and columns of a DataFrame.
+Sample Python dictionary data and list labels:
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew',
+'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+'''
+import numpy as np
+import pandas as pd 
+
+exam = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+                                                'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam, labels)
+print("Number of rows and columns:", df.shape)

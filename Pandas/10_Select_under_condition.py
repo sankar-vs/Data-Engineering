5d2eee72e83348c652ca96979e587733bbdd60bb
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-15 11:42:12 +0530

    [Sankar]Add: Pandas Program - 10

---
+++ Pandas/10_Select_under_condition.py
@@ -0,0 +1,30 @@
+'''
+@Author: Sankar
+@Date: 2021-04-15 11:40:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-15 11:45:09
+@Title : Pandas_Python-10
+'''
+'''
+Write a Python program to select the rows where the number of attempts in the
+examination is greater than 2.
+Sample Python dictionary data and list labels:
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew',
+'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+'''
+import numpy as np
+import pandas as pd 
+
+exam = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+                                                'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam, labels)
+print(df[(df['attempts'] > 2)])

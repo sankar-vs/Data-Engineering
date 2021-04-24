8570a2d8ac4c05ecaddfe542e735dbc96d1f332a
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-24 09:48:38 +0530

    [Sankar]Refactor: indexing

--- RDBMS/Indexing.py
+++ RDBMS/Indexing.py
@@ -4,13 +4,11 @@ from log import logger
 def create_indexing():
     try:
         connect = CRUD() 
-        print("done1")
         cursor = connect._CRUD__db.cursor()
-        print("Done2")
         indexName = input("Enter index name: ")
 
         cursor.excecute("CREATE INDEX {} ON employee_details(name, salary)".format(indexName))
-        print("Done3")
+
         connect._CRUD__db.commit()
 
         logger.info("Index Created: {}".format(indexName))

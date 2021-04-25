f237b9e76d5abf5321f51b0649a21508000fd9fc
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-25 18:37:42 +0530

    [Sankar]Add: Stored Procedures

---
+++ RDBMS/Stored_procedures.py
@@ -0,0 +1,77 @@
+from SQL_Connect import CRUD
+from log import logger
+
+class StoredProcedures():
+
+    def __init__(self):
+        self.connect = CRUD()
+
+    def call_get_merit_student(self):
+        try: 
+            cursor = self.connect._CRUD__db.cursor()
+
+            cursor.callproc('get_merit_student')
+
+            for result in cursor.stored_results():
+                for i in result.fetchall():
+                    logger.info(i)
+
+        except:
+            logger.exception("Call Procedure Aborted")
+
+    def call_get_student_by_limit(self):
+        try: 
+            cursor = self.connect._CRUD__db.cursor()
+
+            cursor.callproc('get_student',[4,])
+
+            for result in cursor.stored_results():
+                for i in result.fetchall():
+                    logger.info(i)
+
+        except:
+            logger.exception("Call Procedure Aborted")
+
+    def call_max_display_marks(self):
+        try: 
+            cursor = self.connect._CRUD__db.cursor()
+
+            result_arg = cursor.callproc('display_max_mark',[1])
+
+            logger.info(result_arg)
+
+        except:
+            logger.exception("Call Procedure Aborted")
+
+    def call_display_marks(self):
+        try: 
+            cursor = self.connect._CRUD__db.cursor()
+
+            result_arg = cursor.callproc('display_marks',[3])
+
+            logger.info(result_arg)
+
+        except:
+            logger.exception("Call Procedure Aborted")
+
+    def show_stored_procedures(self):
+        try: 
+            cursor = self.connect._CRUD__db.cursor()
+
+            cursor.execute("SHOW PROCEDURE STATUS WHERE db = '{}'".format(self.connect._CRUD__database_name))
+
+            result = cursor.fetchall()
+
+            for i in result:
+                logger.info(i)
+
+        except:
+            logger.exception("Show Procedure Aborted")
+
+procedure = StoredProcedures()
+# procedure.call_get_merit_student()
+# procedure.call_get_student_by_limit()
+# procedure.call_max_display_marks()
+# procedure.call_display_marks()
+procedure.show_stored_procedures()
+    

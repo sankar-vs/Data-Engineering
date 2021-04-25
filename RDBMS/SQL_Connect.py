b066f97ad31c86c9b35fe0536a1d2d22e95e2343
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-25 19:07:42 +0530

    [Sankar]Refactor: db correction

--- RDBMS/SQL_Connect.py
+++ RDBMS/SQL_Connect.py
@@ -34,7 +34,7 @@ class CRUD:
         self.__password = config('DB_PASSWORD')
         self.__database_name = config('DB_NAME')
         self.__table_name = config('DB_TABLENAME')
-        self.__db = self.createConnection()
+        self.createConnection()
 
     def createConnection(self):
         '''
@@ -51,7 +51,8 @@ class CRUD:
             
             if (db):
                 logger.info("Connection Successfull!")
-            return db
+                
+            self.__db = db
         except:
             logger.error("Connection Unsuccessfull!")
             

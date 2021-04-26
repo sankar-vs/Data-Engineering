f4b8257c3edd52740f0cf958bbe5708470c60064
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-04-26 10:51:32 +0530

    [Sankar]Refactor: Restructuting for Stored Procedures in MySQL

--- RDBMS/Stored_Procedures/Stored_procedures.py
+++ RDBMS/Stored_Procedures/Stored_procedures.py
@@ -1,8 +1,29 @@
+'''
+@Author: Sankar
+@Date: 2021-04-24 06:46:25
+@Last Modified by: Sankar
+@Last Modified time: 2021-04-26 09:38:09
+@Title : Stored Procedures
+'''
 from SQL_Connect import CRUD
 from log import logger
 
 class StoredProcedures():
-
+    '''
+    Class:
+        StoredProcedures
+    Description:
+        To Call Stored Procedure From the Database.
+    Functions:
+        call_get_merit_student()
+        call_get_student_by_limit()
+        call_max_display_marks()
+        call_display_marks()
+        show_stored_procedures()
+        call_display_students_by_limit()
+    Variable:
+        None
+    '''
     def __init__(self):
         self.connect = CRUD()
 
@@ -38,7 +59,7 @@ class StoredProcedures():
 
             result_arg = cursor.callproc('display_max_mark',[1])
 
-            logger.info(result_arg)
+            logger.info(result_arg[0])
 
         except:
             logger.exception("Call Procedure Aborted")
@@ -49,7 +70,7 @@ class StoredProcedures():
 
             result_arg = cursor.callproc('display_marks',[3])
 
-            logger.info(result_arg)
+            logger.info(result_arg[0])
 
         except:
             logger.exception("Call Procedure Aborted")
@@ -82,12 +103,4 @@ class StoredProcedures():
 
         except:
             logger.exception("Show Procedure Aborted")
-
-procedure = StoredProcedures()
-# procedure.call_get_merit_student()
-# procedure.call_get_student_by_limit()
-# procedure.call_max_display_marks()
-# procedure.call_display_marks()
-# procedure.show_stored_procedures()
-procedure.call_display_students_by_limit()
     
